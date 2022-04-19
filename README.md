e7fe611f3fb34bce063ff25693b0cb2598bb97cb
luciano <luchinjeje@gmail.com>
2022-04-18 22:25:31 -0300

    Added HTTP protocol verbs

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # my-store-node
+
+## install
+
+```sh
+git clone https://github.com/LucianoJavierCostaPeralta/my-store-node.git
+```
+
+```
+cd my-store
+```
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run dev
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
--- index.js
+++ index.js
@@ -1,58 +1,15 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
 const app = express();
 const port = 8080;
+const routerApi = require('./routes/index');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Mi primer server con Express');
 });
 
-app.get('/new-root', (req, res) => {
-  res.send('NEW ROOT');
-});
-
-app.get("/products", (req,res)=>{
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index< limit; index++){
-    products.push({
-      name : faker.commerce.productName(),
-      price : parseInt(faker.commerce.price(),10),
-      image : faker.image.imageUrl(),
-    });
-  }
-  res.json(products);
-});
-
-app.get('/products/filter', (req, res) => {
-  res.send ('Soy un filter')
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({ id });
-});
-
-app.get('/categories/:categoriesId/products/:productsId', (req, res) => {
-  const { categoriesId, productsId } = req.params;
-  res.json({ categoriesId, productsId });
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-
-  {
-    limit && offset
-      ? res.json({
-          limit,
-          offset,
-        })
-      : res.json({
-        mensage : 'No hay parametros'
-      });
-  }
-});
+routerApi(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
